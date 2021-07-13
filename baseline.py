bad44e22ed06b797b8d1a61e5af38deeb0bebbf2
sbsettes <sbsettes>
2021-07-13 13:42:05 +0100

    Latest

--- baseline.py
+++ baseline.py
@@ -17,7 +17,18 @@ import string
 def get_line(id,file_lines):
     for line in file_lines:
         if id in line:
-            return line
+            return line.split(",")[1]
+
+
+# %%
+def clean_corpus(corpus):
+    cleaned_corpus = []
+    for line in corpus:
+        table = str.maketrans(dict.fromkeys(string.punctuation))
+        line = line.translate(table)
+        line = line.lower()
+        cleaned_corpus.append(line)
+    return cleaned_corpus
 
 
 # %%
@@ -27,14 +38,15 @@ def prepare_data(arguments,keypoints,labels):
         matches = []
         for line in labels:
             arg_id = line.split(",")[0]
-            keypoint_id = line.split(",")[1]
-            
+            keypoint_id = line.split(",")[1]            
             match = int(line.split(",")[2])
             argument = get_line(arg_id,arguments)
             keypoint = get_line(keypoint_id,keypoints)
             corpus.append(argument + " " + keypoint)
             matches.append(match)
     matches = array(matches)
+    corpus = clean_corpus(corpus)
+    print("corpus.length == matches.length ? " + str(len(corpus) == len(matches)))
     return corpus,matches
 
 
@@ -44,9 +56,6 @@ def tokenize_and_count(corpus):
     if corpus:
         all_words = []
         for line in corpus:
-            table = str.maketrans(dict.fromkeys(string.punctuation))
-            line = line.translate(table)
-            line = line.lower()
             try:
                 tokenize_word = word_tokenize(line)
             except:
@@ -54,31 +63,19 @@ def tokenize_and_count(corpus):
                 tokenize_word = word_tokenize(line)
             for word in tokenize_word:
                 all_words.append(word)
-        print ("number of words in arguments+keypoints: "+str(len(all_words)))
         unique_words = list(dict.fromkeys(all_words))
         vocab_length = len(unique_words) + 5
-        print ("unique words: " + str(vocab_length))
         return vocab_length
     else:
         print("error while creating corpus")
 
 
 # %%
-def get_padded_sentences(corpus,vocab_length):
+def get_padded_sentences(corpus, vocab_length, length_long_sentence):
     embedded_sentences = [one_hot(sent, vocab_length) for sent in corpus]
-    #print(embedded_sentences )
-
-    # we need the length of each embedded sentence to be the same
-    # so we calculate the length of the longest sentence embedding
-    word_count = lambda sentence: len(word_tokenize(sentence))
-    longest_sentence = max(corpus, key=word_count)
-    length_long_sentence = len(word_tokenize(longest_sentence))
-    print("length of the longest sentence: " + str(length_long_sentence))
-
-    # now we add padding to the sentences that have a length smaller than length_long_sentence
     padded_sentences = pad_sequences(embedded_sentences, length_long_sentence, padding='post')
     print(padded_sentences)
-    return padded_sentences,length_long_sentence
+    return padded_sentences
 
 
 # %%
@@ -108,6 +105,18 @@ def test_model(model,padded_sentences, matches):
 
 
 # %%
+def get_longest_length_sentence(corpus):
+    # we need the length of each embedded sentence to be the same
+    # so we calculate the length of the longest sentence embedding
+    word_count = lambda sentence: len(word_tokenize(sentence))
+    longest_sentence = max(corpus, key=word_count)
+    length_long_sentence = len(word_tokenize(longest_sentence))
+    return length_long_sentence
+
+
+# %%
+print("\n*************************************************************")
+
 # load training data
 with open("./data/train/arguments_train.csv","r",encoding="utf-8") as f:
     arguments_train = f.readlines()[1:]
@@ -115,17 +124,11 @@ with open("./data/train/key_points_train.csv","r",encoding="utf-8") as f:
     keypoints_train = f.readlines()[1:]
 with open("./data/train/labels_train.csv","r",encoding="utf-8") as f:
     labels_train = f.readlines()[1:]
-
 print("keypoints training data size: "+str(len(keypoints_train)))
 print("arguments training data size: "+str(len(arguments_train)))
 print("labels training data size: "+str(len(labels_train)))
-
-# train data embeddings
 corpus_train,matches_train = prepare_data(arguments_train,keypoints_train,labels_train)
-vocab_length_train = tokenize_and_count(corpus_train)
-
-
-# %%
+print("\n--------------------------------------------------------------\n")
 # load test data
 with open("./data/test/arguments_dev.csv","r",encoding="utf-8") as f:
     arguments_dev = f.readlines()[1:]
@@ -133,39 +136,35 @@ with open("./data/test/key_points_dev.csv","r",encoding="utf-8") as f:
     keypoints_dev = f.readlines()[1:]
 with open("./data/test/labels_dev.csv","r",encoding="utf-8") as f:
     labels_dev = f.readlines()[1:]
-
 print("keypoints test data size: "+str(len(keypoints_dev)))
 print("arguments test data size: "+str(len(arguments_dev)))
 print("labels test data size: "+str(len(labels_dev)))
-
-# test data embeddings
 corpus_test,matches_test = prepare_data(arguments_dev,keypoints_dev,labels_dev)
-vocab_length_test = tokenize_and_count(corpus_test)
+
+print("*************************************************************\n")
 
 
-if(vocab_length_test > vocab_length_train):
-    vocab_length_train = vocab_length_test
-else:
-    vocab_length_test = vocab_length_train
+# %%
+vocab_length = max([tokenize_and_count(corpus_train),tokenize_and_count(corpus_test)])
+length_long_sentence = max([get_longest_length_sentence(corpus_train),get_longest_length_sentence(corpus_test)])
+print("max sentence length = " + str(length_long_sentence))
+print("max vocab length = " + str(vocab_length))
 
+# train data embeddings
 print("generating train data embeddings.... ")
-padded_sentences_train,length_long_sentence_train = get_padded_sentences(corpus_train,vocab_length_train)
-
-print("generating test data embeddings.... ")
-padded_sentences_test,length_long_sentence_test = get_padded_sentences(corpus_test,vocab_length_test)
-
-if(length_long_sentence_test > length_long_sentence_train):
-    length_long_sentence_train = length_long_sentence_test
-else:
-    length_long_sentence_test = length_long_sentence_train
+padded_sentences_train = get_padded_sentences(corpus_train,vocab_length,length_long_sentence)
 
 
 # %%
 # training phase
-
-model = create_and_compile_model(vocab_length_train,length_long_sentence_train)
-
+model = create_and_compile_model(vocab_length,length_long_sentence)
 train_model(model,padded_sentences_train,matches_train)
+
+
+# %%
+# test data embeddings
+print("generating test data embeddings.... ")
+padded_sentences_test = get_padded_sentences(corpus_test,vocab_length,length_long_sentence)
 
 
 # %%
